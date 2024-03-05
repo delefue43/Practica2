483abf1bd985f7122e382edd5fd94ef2f298da29
seduut <sergio.duran.utrera.alu@iesfernandoaguilar.es>
2024-03-05 18:26:38 +0100

    modificacion alumno b

--- dadoAleatorio.java
+++ dadoAleatorio.java
@@ -8,7 +8,9 @@ public class dadoAleatorio {
 
         int cara_dado1 = rnd.nextInt(7);
 	int cara_dado2 = rnd.nextInt(7);
-        System.out.println(cara_dado1);
-	System.out.println(cara_dado2);
+        System.out.println("Cara dado 1: " + cara_dado1);
+	System.out.println("Cara dado 2: " +cara_dado2);
+
+	System.out.println("Suma caras: " + (cara_dado1 + cara_dado2));
     }  
 }
