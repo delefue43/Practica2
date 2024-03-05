59d441a83aa70647e258cd92b7b1986c5121ee2f
delefue43 <domingomiguel.elena.fuentes.alu@iesfernandoaguilar.es>
2024-03-05 18:13:00 +0100

    modificacion dado: añade un dado mas

--- dadoAleatorio.java
+++ dadoAleatorio.java
@@ -6,7 +6,9 @@ public class dadoAleatorio {
     
         Random rnd = new Random();
 
-        int cara_dado = rnd.nextInt(7);
-        System.out.println(cara_dado);
+        int cara_dado1 = rnd.nextInt(7);
+	int cara_dado2 = rnd.nextInt(7);
+        System.out.println(cara_dado1);
+	System.out.println(cara_dado2);
     }  
 }
